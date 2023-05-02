2836548cd68f354cead7c2a156c34ac4672fdea5
y6mc37021 <tkarthik21@gmail.com>
2023-05-02 23:46:13 +0100

    Spring University Microservices

---
+++ University/spring-university/src/main/java/edu/university/spring/SpringUniversityApplication.java
@@ -0,0 +1,13 @@
+package edu.university.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUniversityApplication.class, args);
+	}
+
+}
---
+++ University/spring-university/src/test/java/edu/university/spring/SpringUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.university.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
